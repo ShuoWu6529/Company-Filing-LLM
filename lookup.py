ffecdbebf731eb3bb2bbe6b6fbcfb39aa7b399f3
Shuo Wu <sw6529@nyu.edu>
2026-06-13 19:26:42 -0400

    feat: add cik lookup module

---
+++ lookup.py
@@ -0,0 +1,31 @@
+import requests
+
+
+class SecEdgar:
+    def __init__(self):
+        url = "https://www.sec.gov/files/company_tickers.json"
+        headers = {"user-agent": "MLT CP 28 shuowu1000@gmail.com"}
+        r = requests.get(url, headers=headers)
+        filejson = r.json()
+
+        self.name_dict = filejson.copy()
+        for old_key, value in filejson.items():
+            new_key = value["title"]
+            self.name_dict[new_key] = self.name_dict.pop(old_key)
+
+        self.ticker_dict = filejson.copy()
+        for old_key, value in filejson.items():
+            new_key = value["ticker"]
+            self.ticker_dict[new_key] = self.ticker_dict.pop(old_key)
+
+    def name_to_cik(self, name):
+        company_info = self.name_dict[name]
+        cik = company_info["cik_str"]
+        ticker = company_info["ticker"]
+        return (cik, name, ticker)
+
+    def ticker_to_cik(self, ticker):
+        company_info = self.ticker_dict[ticker]
+        cik = company_info["cik_str"]
+        name = company_info["title"]
+        return (cik, name, ticker)
