f04762750d8ff0eda8962fb440715a4456dbf757
Shuo Wu <sw6529@nyu.edu>
2026-06-28 23:35:58 -0400

    feat: initialize SAM project with EDGAR refresh Lambda

---
+++ edgar_refresh/app.py
@@ -0,0 +1,21 @@
+import json
+import os
+from cik_lookup import SecEdgar
+import boto3
+
+
+def lambda_handler(event, context):
+    sec = SecEdgar()
+    sec.load_from_sec()
+    s3_client = boto3.client('s3')
+
+    response = s3_client.put_object(
+        Bucket=os.environ["BUCKET_NAME"],
+        Key="company_tickers.json",
+        Body=json.dumps(sec.raw_tickers)
+    )
+
+    return {
+        "statusCode": 200,
+        "body": "Successfully updated SEC ticker data!",
+    }
